62798190d1207c50df78f3e90e9a4e365783e306
Natsuki Kawai <natsuki.kawai@gmail.com>
2011-11-06 01:42:02 +0900

    Implemented add methods of RandomReadable.

--- lib/random-accessible.rb
+++ lib/random-accessible.rb
@@ -1,6 +1,6 @@
 module RandomAccessable
 
-  module CommonFunctions
+  module CommonTraits
 
     def each_index(&block)
       if block.nil?
@@ -17,11 +17,34 @@ module RandomAccessable
     end
 
     def empty?
-      # Return false if size method is not implemented.
-      size <= 0
+      if has_size?
+        size <= 0
+      else
+        false
+      end
     end
 
-    # TODO: Implement size and length
+    def has_size?
+      return method(:size).owner != CommonTraits ||
+             method(:length).owner != CommonTraits
+    end
+    private :has_size?
+
+    def size
+      if method(:length).owner == CommonTraits
+        raise NotImplementedError,
+              "#{self.class.to_s} has neither length method nor size method."
+      end
+      return length
+    end
+
+    def length
+      if method(:size).owner == CommonTraits
+        raise NotImplementedError,
+              "#{self.class.to_s} has neither length method nor size method."
+      end
+      return size
+    end
 
   end
 
@@ -29,7 +52,15 @@ end
 
 module RandomReadable
 
+  include RandomAccessible::CommonTraits
+  include Enumerable
+
   def to_ary
+    unless has_size?
+      raise NotImplementedError,
+            "#{self.class.to_s} has neither length method nor size method."
+    end
+
     Enumerator.new do |y|
       size.times do |i|
         y << at(i)
@@ -40,27 +71,25 @@ module RandomReadable
   alias :to_a :to_ary
 
   def deligate_to_array(name, *args)
-    if Array.public_method_defined?(name)
-      return to_ary.send(name, *args)
-    else
-      super(name, *args)
-    end
+    to_ary.send(name, *args)
   end
   private :deligate_to_array
-  alias :method_missing :deligate_to_array
 
-  #def &(other)
-  #end
+  def &(other)
+    deligate_to_array(:&, other)
+  end
 
-  #def *(arg)
-  #end
+  def *(arg)
+    deligate_to_array(:*, arg)
+  end
 
-  #def +(other)
-  #end
+  def +(other)
+    deligate_to_array(:+, other)
+  end
 
-  #def -(other)
-  #end
-
+  def -(other)
+    deligate_to_array(:-, other)
+  end
 
   def <=>(other)
     size = [size, other.size].min
@@ -72,6 +101,7 @@ module RandomReadable
   end
 
   def ==(other)
+    return super unless has_size?
     return false if size != other.size
     size.times do |i|
       return false if self[i] != other[i]
@@ -81,7 +111,8 @@ module RandomReadable
   end
 
   def assoc(key)
-    each do |el|
+    enum = has_size? ? each : cycle
+    enum.each do |el|
       if el.respond_to?(:[]) && !el.empty? && el[0] == key
         return el
       end
@@ -92,12 +123,11 @@ module RandomReadable
 
   # Do not override Object#clone and Object#dup
 
-  #def combination(n)
-  #end
+  def combination(n)
+    deletage_to_array(:combination, n)
+  end
 
   def compact
-    return @substance.compact if @substance
-
     Enumerator.new do |y|
       each do |el|
         y << el unless el.nil?
@@ -105,8 +135,17 @@ module RandomReadable
     end.to_a
   end
 
-
-  # self#cycle is defined in Enumerable.
+  def cycle
+    if has_size?
+      super
+    else
+      i = 0
+      loop do
+        yield at(i)
+        i += 1
+      end
+    end
+  end
 
   def each(&block)
     if block.nil?
@@ -123,23 +162,59 @@ module RandomReadable
     end
   end
 
-  def eql?
-    #
+  def eql?(other)
+    return super(other) unless has_size?
+
+    each_index do |i|
+      return false unless at(i).eql?(other.at(i))
+    end
+    return true
   end
 
   def fetch(nth, *args, &block)
-    #
+    if args.size >= 2
+      raise ArgumentError, "wrong number of arguments (#{args.size + 1} for 1..2)"
+    end
+
+    if nth < -size || size <= nth
+      if block != nil
+        # TODO: Warn if ifnone value is present.
+        return block.call
+      elsif args.length == 1
+        return args[0]
+      else
+        raise IndexError,
+              "index #{nth} outsize of the random readable object " \
+              "bounds: #{-size}...#{size}"
+      end
+    else
+      return at(nth)
+    end
   end
 
-  def first(n = 0)
-    #
+  def first(*args)
+    if args.size >= 2
+      raise ArgumentError, "wrong number of arguments (#{args.size + 1} for 1..2)"
+    end
+
+    if args.size == 1
+      width = args[0]
+      width = [width, size].max if has_size?
+ 
+      return self[0...width]
+    else
+      return at(0)
+    end
   end
 
-  #def flatten
-  #end
+  def flatten
+    delegate_to_array(:flatten)
+  end
 
   def hash
-    #
+    return super unless has_size?
+
+    delegate_to_array(:hash)
   end
 
   def include?(val = nil, &block)
@@ -163,22 +238,22 @@ module RandomReadable
   # indexes is not defined on Ruby 1.9.
 
   def to_s
-    # Needs size.
+    return super unless has_size?
+
     to_ary.to_s
   end
 
   def inspect
-    # Needs size.
+    return super unless has_size?
+
     to_ary.inspect
   end
 
   def join(sep = $,)
-    # Needs size.
     to_ary.join(sep)
   end
 
   def last(n = nil)
-    # Needs size.
     if n.nil?
       at(size - 1)
     else
@@ -190,15 +265,18 @@ module RandomReadable
     end
   end
 
-  #def pack(template)
-  # TODO: Implement lazy eval.
-  #end
+  def pack(template)
+    #TODO: Implement lazy eval.
+    delegate_to_array(:pack, template)
+  end
 
-  #def permutation(n)
-  #end
+  def permutation(n)
+    delegate_to_array(:permutation, n)
+  end
 
-  #def product(*lists, &block)
-  #end
+  def product(*lists, &block)
+    delegate_to_array(:product, *lists, &block)
+  end
 
   def rassoc(obj)
     each do |el|
@@ -209,14 +287,17 @@ module RandomReadable
     return nil
   end
 
-  #def repeated_combination(n, &block)
-  #end
+  def repeated_combination(n, &block)
+    delgate_to_array(:repeated_combination, n, &block)
+  end
 
-  #def repeated_permutation(n, &block)
-  #end
+  def repeated_permutation(n, &block)
+    delegate_to_array(:repeated_permutation, n, &block)
+  end
 
-  #def reverse
-  #end
+  def reverse
+    delegate_to_array(:reverse)
+  end
 
   def reverse_each(&block)
     # Needs size.
@@ -246,15 +327,52 @@ module RandomReadable
     return nil
   end
 
-  #def rotate(cnt = 1)
-  #end
-
-  def sample(n = nil)
-    #
+  def rotate(cnt = 1)
+    delegate_to_array(:rotate, cnt)
   end
 
-  #def shuffle
-  #end
+  def sample(*args)
+    # Needs size.
+    if args.size >= 2
+      raise ArgumentError, "wrong number of arguments (#{args.size + 1} for 1..2)"
+    end
+
+    if args.size == 1
+      n = [args[0].to_int, size].max
+      return Enumerator.new do |y|
+        each_with_index do |el, i|
+          if n > 0 && rand(size - i) <= n
+            y << el
+            n -= 1
+          end
+        end
+      end.to_i
+    else
+      if size == 0
+        return nil
+      else
+        return at(rand(size))
+      end
+    end
+  end
+
+  def shuffle
+    delegate_to_array(:shuffle)
+  end
+
+  alias :slice :[]
+
+  def sort(&block)
+    delegate_to_array(:sort, &block)
+  end
+
+  def transpose
+    delegate_to_array(:transpose)
+  end
+
+  def uniq
+    delegate_to_array(:uniq)
+  end
 
 end
 
@@ -279,6 +397,18 @@ module RandomWritable
 
   def concat
     #
+  end
+
+  def cycle
+    if has_size?
+      super
+    else
+      i = 0
+      loop do
+        yield at(i)
+        i += 1
+      end
+    end
   end
 
   def fill
@@ -380,7 +510,35 @@ module RandomAccessible
   end
 
   def shuffle!
+    # TODO: Optimize me.
     replace(shuffle)
+  end
+
+  def slice!(*args)
+    result = self[*args]
+    self[*args] = nil
+    result
+  end
+
+  def sort!(&block)
+    # TODO: Optimize me.
+    replace(sort(&block))
+  end
+
+  def sort_by!(&block)
+    # TODO: Optimize me.
+    replace(sort_by(&block))
+  end
+
+  def uniq!(&block)
+    replace(uniq(&block))
+  end
+
+  def unshift(*obj)
+    # TODO: Optimize me.
+    obj.each do |el|
+      insert_at(0, el)
+    end
   end
 
 end
